4a001bc7dc5c47d3d9e22fe54e000393b6d1ee09
Piotr Ptaszek <167607225+PITERPTA@users.noreply.github.com>
2024-04-20 09:48:07 +0200

    Update fraktal.py

--- fraktal.py
+++ fraktal.py
@@ -1,11 +1,10 @@
-#instalacja: pip install matplotlib
+
 import matplotlib.pyplot as plt     
 import random as rnd
-x = [0]     # lista x z wprowadzonym x początkowym x_0 = 0
-y = [0]     # lista y z wprowadzonym y początkowym y_0 = 0
+x = [0]     0
+y = [0]     
 for i in range(1, 10000):
-    random_number = rnd.random()    #liczba rzeczywista z przedziału [0;1)
-    # jeśli wylosowana liczba zawiera się w [0;0.5), to wybierany jest pierwszy układ równań
+    random_number = rnd.random()    
     if random_number < 0.1:         
         x.append(0.14 * x[i - 1] + 0.01 * y[i - 1] - 1.31)
         y.append(0.0 * x[i - 1] + 0.51 * y[i - 1] + 0.1)
@@ -22,5 +21,5 @@ for i in range(1, 10000):
             else:
                 x.append(0.49 * x[i - 1] + 0.0 * y[i - 1] + 0.02)
                 y.append(0.0 * x[i - 1] + 0.51 * y[i - 1] + 1.62)
-plt.plot(x, y, ".", markersize=1)      #rozmiar punktu wielkości 1
+plt.plot(x, y, ".", markersize=1)      
 plt.show()
