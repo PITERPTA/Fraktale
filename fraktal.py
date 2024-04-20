e48591cab26e8c627ec09c0a656842a8b3be616e
Piotr Ptaszek <167607225+PITERPTA@users.noreply.github.com>
2024-04-20 09:53:22 +0200

    Update fraktal_liść.py

--- fraktal.py
+++ fraktal.py
@@ -1,4 +1,3 @@
-
 import matplotlib.pyplot as plt     
 import random as rnd
 x = [0]     0
@@ -18,8 +17,10 @@ for i in range(1, 10000):
             if random_number < 0.8:
                 x.append(0.45 * x[i - 1] - 0.49 * y[i - 1] - 1.62)
                 y.append(0.47 * x[i - 1] + 0.47 * y[i - 1] - 0.74)
+                
             else:
                 x.append(0.49 * x[i - 1] + 0.0 * y[i - 1] + 0.02)
                 y.append(0.0 * x[i - 1] + 0.51 * y[i - 1] + 1.62)
+
 plt.plot(x, y, ".", markersize=1)      
 plt.show()
